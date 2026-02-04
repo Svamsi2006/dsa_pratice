1af42d013cac4bae46db65e490a218a94a543af9
vamsi siva ganesh seelam <84583325+Svamsi2006@users.noreply.github.com>
2026-02-04 12:34:35 +0530

    matrices updated

---
+++ matrices.c
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int arr[3][3];
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cin>>arr[i][j];
+        }
+    }
+    vector<int> hi,bye;
+    for(int i=0;i<3;i++){
+        int max=arr[i][0];
+        int min=arr[i][0];
+        for(int j=0;j<3;j++){
+            cout<<arr[i][j]<<" ";
+            if(max<arr[i][j]){
+                max=arr[i][j];
+            }
+            if(min>arr[i][j]){
+                min=arr[i][j];
+            }
+        }
+        hi.push_back(max);
+        bye.push_back(min);
+        cout<<endl;
+    }
+      cout<<endl;
+    for(int i:hi){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    for(int i:bye){
+        cout<<i<<" ";
+    }
+    
+}
